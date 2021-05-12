e0f09f3d6ca13d17b5b6512976c98ed6693cca9e
Gioele Pannetto <gioele@pannetto.com>
2021-05-12 16:31:02 +0200

    Logout

--- resources/views/pages/admin/home.blade.php
+++ resources/views/pages/admin/home.blade.php
@@ -2,5 +2,5 @@
 @section('seo::title', 'Dashboard')
 
 @section('app')
-    Benvenuto {{ Auth::user()->name }}
+    Benvenuto {{ Auth::user()->name }}. Non sei tu? <a href="{{ route('logout') }}">sloggati</a>
 @endsection
--- routes/admin.php
+++ routes/admin.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DashboardController@home');
+Route::get('/logout', 'DashboardController@logout')->name('logout');
