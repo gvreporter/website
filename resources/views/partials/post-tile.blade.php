dbe44346071e814475bbe400f8cecf7f76a7f0eb
Gioele Pannetto <gioele@pannetto.com>
2021-05-12 20:45:48 +0200

    Basic styling

--- resources/views/partials/post-tile.blade.php
+++ resources/views/partials/post-tile.blade.php
@@ -1,7 +1,13 @@
+@php
+    $url = route('posts::show', $post->slug);
+@endphp
+
 <article class="post">
-    <div class="post_img" style="background-image: url({{ $post->cover_url }})"></div>
+    <a href="{{ $url }}" title="Vai all'articolo">
+        <div class="post_img" style="background-image: url({{ $post->cover_url }})"></div>
+    </a>
     <div class="post_info">
-        <span class="post_title">{{ $post->title }}</span>
+        <a href="{{ $url }}" title="Vai all'articolo"><span class="post_title">{{ $post->title }}</span></a>
         @auth
             <span class="post_author">{{ $post->views }} views</span>
         @endauth
