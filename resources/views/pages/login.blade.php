b827e3f08e65c3c00c7fe340989165de79d06d3e
Gioele Pannetto <gioele@pannetto.com>
2021-05-15 15:24:41 +0200

    Basic login styling

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -2,13 +2,37 @@
 @section('seo::title', 'Login')
 
 @section('app')
-    <form action="{{ route('login') }}" method="post">
-        @csrf
-        <input type="username" placeholder="Username" name="username" value="{{ old('username') }}" required />
-        <input type="password" placeholder="Password" name="password" required />
-        @error('password')
-            {{ $message }}
-        @enderror
-        <input type="submit" value="LOGIN">
-    </form>
+    <div class="login">
+        <div class="login_container">
+            <h3>Accedi</h3>
+            <span>Inserisci le tue credenziali da staff</span>
+            <form action="{{ route('login') }}" method="post">
+                @csrf
+                <div class="login_form">
+                    <input 
+                        class="login_form_field"
+                        type="text"
+                        autocomplete="username"
+                        placeholder="Username"
+                        name="username"
+                        value="{{ old('username') }}"
+                        required
+                    />
+                    <input
+                        class="login_form_field"
+                        type="password"
+                        autocomplete="current-password"
+                        placeholder="Password"
+                        name="password"
+                        required
+                    />
+                </div>
+                @error('password')
+                    {{ $message }}
+                @enderror
+                <input type="submit" value="LOGIN">
+            </form>
+            <span>Non fai parte dello staff? Loggati <a href="{{ route('oauth::login') }}">qui</a> con il tuo account istituzionale</span>
+        </div>
+    </div>
 @endsection
